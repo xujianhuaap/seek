ef6e2e1669aef71708d3a0d43a74ebb5ab7a1a3e
skullmind <xujianhuaap@gmail.com>
2018-04-05 22:25:28 +0800

    queryTable

--- controllers/product_list_controller.go
+++ controllers/product_list_controller.go
@@ -4,7 +4,8 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	"strconv"
-	"fmt"
+	"github.com/xujianhuaap/seek/net"
+	"encoding/json"
 )
 
 type ProductListController struct {
@@ -23,12 +24,22 @@ type Product struct {
 
 
 func (this * ProductListController)Get()  {
-	
-	this.Ctx.WriteString("welcome to seek.com")
+	var products []*Product
+	o:= orm.NewOrm()
+	_,err:=o.QueryTable(new(Product)).All(&products)
+	var resp net.Response
+	if err != nil {
+		resp=net.Response{1,err.Error(),net.DataNil{}}
+	}else {
+		resp=net.Response{0,"",products}
+	}
+	json,err:= json.Marshal(resp)
+	defer this.Ctx.WriteString(string(json))
 }
 
 
 func (this * ProductListController)Post()  {
+
 	product_name:=this.Ctx.Request.Form.Get("name")
 	product_price := this.Ctx.Request.Form.Get("price")
 	product_sale_price:= this.Ctx.Request.Form.Get("sale_price")
@@ -47,9 +58,15 @@ func (this * ProductListController)Post()  {
 	product.Price = price
 	product.SalePrice = sale_price
 	o:= orm.NewOrm()
-
-	fmt.Println(o.Insert(product))
-	this.Ctx.WriteString("OK")
+	index,err:=o.Insert(product)
+	var resp net.Response
+	if err!= nil{
+		resp=net.Response{1,err.Error(),"create failrue"}
+	}else {
+		resp=net.Response{0,"",index}
+	}
+	json,_:=json.Marshal(resp)
+	this.Ctx.WriteString(string(json))
 
 }
 
